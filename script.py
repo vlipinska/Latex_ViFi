fb56cc550d1cb9a6f928d26a31f4add70e5c38f4
vlipinska <v.lipinska@tudelft.nl>
2017-11-10 13:59:39 +0100

    script: checking for updates

--- script.py
+++ script.py
@@ -1,13 +1,26 @@
-import os  
+import os, time
 
-print("Compiling .tex files...")
-os.system("pdflatex *.tex")
-print("Done.")
+path_to_watch = "."
+before = dict ([(f, None) for f in os.listdir(path_to_watch)])
 
-print("Movind .pdf files...")
-os.system("mv *.pdf ./pdfs")
-print("Done.")
+while 1:
+    time.sleep (10)
+    after = dict ([(f, None) for f in os.listdir(path_to_watch)])
+    added = [f for f in after if not f in before]
+    removed = [f for f in before if not f in after]
+    if added: print("Added: ", ", ".join (added))
+    if removed: print("Removed: ", ", ".join (removed))
+    before = after
 
-print("Removing auxiliary files...")
-os.system("rm *.log *.aux")
-print("Done.")
+    print("Compiling .tex files...")
+    os.system("pdflatex *.tex")
+    print("Done.")
+
+    print("Movind .pdf files...")
+    os.system("mv *.pdf ./pdfs")
+    print("Done.")
+
+    print("Removing auxiliary files...")
+    os.system("rm *.log *.aux")
+    print("Done.")
+
